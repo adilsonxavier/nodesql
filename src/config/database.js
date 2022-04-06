6125f952335e59480d2aa7d7983bdea623f32e0f
adilsonxdesouza <adilsonxdesouza@hotmail.com>
2022-04-06 11:41:21 -0300

    conexão bd e migrations ok

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+﻿module.exports = {
+    dialect: 'mssql',
+    host: 'localhost',
+    username: 'sa',
+    password: 'sqlserver',
+    database: 'sqlnode',
+    define: {
+        timestamps: true,  // Cria autom campos created_at e updated_at
+      //  underscored: true,
+    },
+};
+
